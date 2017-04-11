b505aebe61c5e8db975d1392de01138105b0e7f7
Roberto Russo <r.russo@arca.com>
2017-04-11 11:58:28 +0200

    Added new methods

--- src/github.ts
+++ src/github.ts
@@ -1,41 +1,71 @@
 const https = require("https");
+const querystring = require("querystring");
 
 class Github {
     readonly GITHUB_ENDPOINT = "api.github.com";
     public request = {};
+
     public constructor() {
         this.request = {
             hostname: this.GITHUB_ENDPOINT,
-            path: "",
+            path: "/",
             headers: {
                 "user-agent": "node.js"
             },
-            method: ""
+            method: null,
+            auth: null
         };
     }
-    public listUserRepositories = function (username) {
-        return new Promise((resolve, reject) => {
-            this.request.path = "/users/" + username + "/repos";
-            this.request.method = "GET";
-            let body = "";
 
+    /**
+     * Repositories
+     */
+
+    /** 
+     * Provides an array of the user's repository of the 
+     * @async 
+     */
+    public listUserRepositories = function (user, parameters) {
+        this.request.path = "/users/" + user + "/repos?" + querystring.stringify(parameters);
+        this.request.method = "GET";
+        return this.promise();
+    };
+
+    public listYourRepositories = function (parameters) {
+        this.request.path = "/user/repos?" + querystring.stringify(parameters);
+        this.request.method = "GET";
+        return this.promise();
+    };
+
+    public setCredentials = function (user, password) {
+        let auth = "Basic " + new Buffer(user + ":" + password).toString("base64");
+        this.request.auth = user + ":" + password;
+    };
+
+    public unsetCredentials = function () {
+        this.request["auth"] = null;
+    };
+
+    private promise = function () {
+        return new Promise((resolve, reject) => {
+            let body = "";
             let req = https.request(this.request, (res) => {
                 res.on("data", (chunk) => {
                     body += chunk.toString("utf8");
                 });
                 res.on("end", () => {
-                    return resolve(body)
+                    return resolve(body);
                 });
             });
             req.on("error", (e) => {
-                return reject(e)
+                return reject(e);
             });
             req.end();
         });
     };
 }
 
-let github = new Github()
+let github = new Github();
 export {
     github as Github
 }
